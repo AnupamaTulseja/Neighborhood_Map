f14c1b033e9505c81410ada599c5dc4aae8f8889
AnupamaTulseja <32341495+AnupamaTulseja@users.noreply.github.com>
2018-02-25 18:39:51 -0800

    Add files via upload

---
+++ js/markers.js
@@ -0,0 +1,100 @@
+// Default Locations that are displayed on the map
+
+var myLocations = [
+    {
+        title: 'Citra Apartments',
+        lat: 37.371520,
+        lng: -122.057395,
+        type: 'Residence'
+    },
+    {
+        title: 'Satkar Restaurant',
+        lat: 37.375617,
+        lng: -122.058117,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Himalaya Kitchen',
+        lat: 37.377413,
+        lng: -122.062579,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Arbys',
+        lat: 37.373660,
+        lng: -122.056923,
+        type: 'Restaurant'
+    },
+    {
+        title: 'AappaKadai',
+        lat: 37.37332,
+        lng: -122.052300,
+        type: 'Restaurant'
+    },
+    {
+        title: 'First Wok',
+        lat: 37.372210,
+        lng: -122.058066,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Ross',
+        lat: 37.367474,
+        lng: -122.029506,
+        type:  'Shopping'
+    },
+    {
+        title: 'Westfield Valley Fair',
+        lat: 37.325270,
+        lng: -121.945704,
+        type: 'Shopping'
+    },
+    {
+        title: 'Felipe Market',
+        lat: 37.373166,
+        lng: -122.052347,
+        type: 'Grocery'
+    },
+    {
+        title: 'First Wok',
+        lat: 37.372819,
+        lng: -122.058115,
+        type: 'Grocery'
+    },
+    {
+        title: 'Mountain View Station',
+        lat: 37.396733,
+        lng: -122.077174,
+        type:  'Transit'
+    },
+    {
+        title: 'Philz Coffee',
+        lat: 37.377396,
+        lng: -122.031400,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Dishdash',
+        lat: 37.376186,
+        lng: -122.030135,
+        type: 'Restaurant'
+    },
+    {
+        title: 'In-N-Out Burger',
+        lat: 37.360988,
+        lng: -122.024966,
+        type: 'Restaurant'
+    },
+    {
+        title: 'Cherry Chase Elementary School',
+        lat: 37.366497,
+        lng: -122.056604,
+        type: 'Education'
+    },
+    {
+        title: 'Cumberland Elementary School',
+        lat: 37.367002,
+        lng: -122.043286,
+        type: 'Education'
+    },
+]
